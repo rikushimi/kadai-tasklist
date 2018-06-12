82749fe669eefd4788b7b5826b1c9c46f8cbde8b
riku.shimizu <ec2-user@ip-172-31-2-147.us-east-2.compute.internal>
2018-06-12 03:32:45 +0000

    perfetly

--- database/migrations/2018_06_08_170308_add_userid_to_tasks_table.php
+++ database/migrations/2018_06_08_170308_add_userid_to_tasks_table.php
@@ -14,7 +14,7 @@ class AddUseridToTasksTable extends Migration
     public function up()
     {
         Schema::table('tasks', function (Blueprint $table) {
-            $table->integer('user_id');
+            $table->integer('user_id')->unsigned()->index();
             $table->foreign('user_id')->references('id')->on('users');
         });
     }
